bb0d8903c974b551abe93d5a3db0062970d2cc37
sudeshsubedi <programmedevil13@gmail.com>
2022-09-08 19:18:18 +0545

    new function for sfml project

---
+++ automation.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/bash
+
+git_auto() {
+    if [ $# -ne 1 ]
+    then
+        echo "Usage: git_auto message"
+        return 1
+    fi
+
+    git add -A
+    git commit -m "$1"
+    git push origin master
+}
+
+sfml_auto() {
+    if [ $# -ne 1 ]
+    then
+        echo "Usage: sfml_auto output_name"
+        return 1
+    fi
+
+
+    FILES=""
+    for i in $(find -type f -name "*.cpp"); do
+        FILES+=" "
+        FILES+=$i
+    done
+    g++ $FILES -o builds/$1 -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio -lsfml-network    
+}
+
+
+alias cmaked="cmake -B builds && cd builds && make && cd .."
